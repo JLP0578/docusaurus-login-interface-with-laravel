10eef86968f021ad535c6a1f52669bbdebafb27a
JLP0578 <54902345+JLP0578@users.noreply.github.com>
2025-01-07 09:12:16 +0100

    init

---
+++ docusaurus.config.js
@@ -0,0 +1,58 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import "dotenv/config";
+import { themes as prismThemes } from "prism-react-renderer";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Your Title",
+  // tagline: 'Dinosaurs are cool',
+  favicon: "",
+
+  // Set the production url of your site here
+  url: "http://your-url.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+  // Permet d'utiliser le '/login'
+  trailingSlash: false,
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "your-organization-name", // Usually your GitHub org/user name.
+  projectName: "your-project-name", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
+
+  customFields: {
+    AppUrl: process.env.APP_URL,
+    TimeBeforeNewValidation: process.env.TIME_BEFORE_NEW_VALIDATION,
+  },
+
+  presets: [
+    ...
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    (...),
+};
+
+export default config;
